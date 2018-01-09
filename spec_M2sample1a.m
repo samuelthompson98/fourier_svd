e31d08f32bd9e0b962f48d43393f9d3a2584cd89
Samuel Thompson <u6083481@anu.edu.au>
2018-01-09 16:46:09 +1100

    Fiddled with code

--- spec_M2sample1a.m
+++ spec_M2sample1a.m
@@ -11,11 +11,9 @@ addpath svd_scripts
 % last item gets popped to top of stack....
 % addpath /home/hol105/MHD_codes/matlab/svd_scripts
 
-
 load struc_xmd.mat
 
 % MJH processing 15/06/07: remove mean
-
 dt   = 5.0e-7; % 0.5 mus
 tmax = 0.3;
 
@@ -29,6 +27,9 @@ n1 = 10;
 A2 = 1;
 f2 = 100e+3;
 n2 = -10;
+A3 = 1e5;
+f3 = 150e+3;
+n3 = 5;
 
 xmd.omt(1).signal(:,2) = A1 * cos(xmd.omt(1).signal(:,1) * 2* pi *f1 + n1 * xmd.omt(1).phi )  + A2 * cos(xmd.omt(1).signal(:,1).^2 * 2* pi *f2 + n2 * xmd.omt(1).phi ); 
 xmd.omt(2).signal(:,2) = A1 * cos(xmd.omt(2).signal(:,1) * 2* pi *f1 + n1 * xmd.omt(2).phi )  + A2 * cos(xmd.omt(2).signal(:,1).^2 * 2* pi *f2 + n2 * xmd.omt(2).phi ); 
@@ -44,14 +45,13 @@ end
 % add pink noise taken from
 % https://ccrma.stanford.edu/~jos/sasp/Example_Synthesis_1_F_Noise.html
 
-
 Nx = 2^16;  % number of samples to synthesize
 Nx = Nt;  
 B = [0.049922035 -0.095993537 0.050612699 -0.004408786];
 A = [1 -2.494956002   2.017265875  -0.522189400];
 nT60 = round(log(1000)/(1-max(abs(roots(A))))); % T60 est.
 
-beta = 100.0 %0.1;
+beta = 0.05; %Breaks for beta = 0.01 and below
 for i=1:3
     v = randn(1,Nx+nT60); % Gaussian white noise: N(0,1)
     x = filter(B,A,v);    % Apply 1/F roll-off to PSD
@@ -64,17 +64,18 @@ winl    = 4096
 winl    = 2048
 norm    = spec_norm(winl)
 
-
 disp(['Spectrum ======================================']);
 XMD.omt = spec(xmd.omt, winl, norm);
 
 tnorm = 1e-3
 fnorm = 1e+3
-%This function gives an error
+%This function gives an error - had to change some content
 render_example(XMD.omt(1), tnorm, fnorm) %Figure 1
 
 it = min(find(XMD.omt(1).t >= 0.165))
 figure
+size(XMD.omt(1).f)
+size(abs(XMD.omt(1).F(:,it)))
 semilogy(XMD.omt(1).f, abs(XMD.omt(1).F(:,it))) %Figure 2
 
 toff   = min(xmd.omt(1).signal(:,1));
