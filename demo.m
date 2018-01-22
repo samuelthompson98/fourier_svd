e0002cbabc78dcb0a1af25b2cfdb040cc352bc0d
Samuel Thompson <u6083481@anu.edu.au>
2018-01-22 14:26:02 +1100

    Fixed mode number compraison

--- demo.m
+++ demo.m
@@ -16,7 +16,7 @@ for i = 1:3
 end
 
 %Linearly increasing frequency
-A1 = 5;
+A1 = 0;%5;
 f1 = 12.5e+4;
 n1 = 15;
 %Constant frequency
@@ -43,7 +43,7 @@ A = [1.0 -2.494956002   2.017265875  -0.522189400];
 disp(max(abs(roots(A))));
 nT60 = round(log(1000)/(1-max(abs(roots(A))))); % T60 est.
 
-beta = 0.1;
+beta = 0.0;
 for i=1:3
     v = randn(1,Nx+nT60); % Gaussian white noise: N(0,1)
     x = filter(B,A,v);    % Apply 1/F roll-off to PSD
@@ -75,40 +75,45 @@ semilogy(XMD.omt(1).f, abs(XMD.omt(1).F(:,it))) %Figure 2
 toff   = min(xmd.omt(1).signal(:,1));
 
 get_frequency = @(t) 2 * f1 * t;
-times = 0:0.05:0.29
+times = 0.0:0.01:0.29
 plot_amplitude_and_mode_number_relative_differences(times', XMD.omt, get_frequency, A1, n1);
 
+%{
 % M=1 mode
-%[Z1]    = nmode(XMD.omt,0.2,1,500,100e+3) %0.165
-%Z1.shot = 9429
-%[ Z1_noise ]  = fit_mag_power3( Z1) %Figure 3
-%pltn_M1data(Z1, Z1_noise); %Figure 4
+[Z1]    = nmode(XMD.omt,0.2,1,500,100e+3) %0.165
+Z1.shot = 9429
+[ Z1_noise ]  = fit_mag_power3( Z1) %Figure 3
+pltn_M1data(Z1, Z1_noise); %Figure 4
 
 % M=2 mode
-%[Z2]    = nmode(XMD.omt,0.15,2,500,100e+3)
-%Z2      = nmode_filter(Z2);
+[Z2]    = nmode(XMD.omt,0.15,2,500,100e+3)
+Z2      = nmode_filter(Z2);
 
-%Z2.shot = 9429
-%[ Z2_noise ]  = fit_mag_power3( Z2) %Figure 5
+Z2.shot = 9429
+[ Z2_noise ]  = fit_mag_power3( Z2) %Figure 5
 
-%pltn_M2data(Z2, Z2_noise); %Figure 6
+pltn_M2data(Z2, Z2_noise); %Figure 6
 
-%[Z21]    = nmode(XMD.omt,0.25,2,500,100e+3)
-%Z21      = nmode_filter(Z21);
-%Z21.shot = 9429
-%[ Z21_noise ]  = fit_mag_power3( Z21) %Figure 7
+[Z21]    = nmode(XMD.omt,0.25,2,500,100e+3)
+Z21      = nmode_filter(Z21);
+Z21.shot = 9429
+[ Z21_noise ]  = fit_mag_power3( Z21) %Figure 7
 
-%pltn_M2data(Z21, Z21_noise); %Figure 8
+pltn_M2data(Z21, Z21_noise); %Figure 8
 
-%ts = [0.11; 0.15; 0.18; 0.2; 0.22; 0.25; 0.29]
+ts = [0.11; 0.15; 0.18; 0.2; 0.22; 0.25; 0.29]
+%}
 
-for i = 1:size(times')
+%%{
+for i = 1:1%size(times')
     [Z2]    = nmode(XMD.omt,times(i),2,500,100e+3)
+    1e5 * max(abs(Z2.a(:, 1)))
     Z2      = nmode_filter(Z2);
     Z2.shot = 9429
     [ Z2_noise ]  = fit_mag_power3( Z2)
     pltn_M2data(Z2, Z2_noise);
 end
+%%}
 
 save struc_XMD.mat XMD
 
--- svd_scripts/plot_amplitude_and_mode_number_relative_differences.m
+++ svd_scripts/plot_amplitude_and_mode_number_relative_differences.m
@@ -7,13 +7,20 @@ function plot_amplitude_and_mode_number_relative_differences(times, spectrogram,
     relative_n_differences = zeros(size(times));
     
     for i = 1:size(times)
-        [mode_object] = nmode(spectrogram, times(i), 2, 500, 100e+3); %Adjust these parameters
+        [mode_object] = nmode(spectrogram, times(i), 2, 500, 100e+3); %Adjust these parameters maybe
+        mode_object = nmode_filter(mode_object)
+        %{
+        mode_object.shot = 9429
+        [ mode_object_noise ]  = fit_mag_power3( mode_object)
+        pltn_M2data(mode_object, mode_object_noise);
+        %}
+        
         frequency = frequencies(i)
         frequency_differences = abs(frequency - mode_object.f);
         index = find(frequency_differences == min(frequency_differences))
        
-        fitted_amplitude = abs(mode_object.a(index, 1));
-        fitted_n = abs(mode_object.n(index, 1))
+        fitted_amplitude = abs(mode_object.a(index, 1))
+        fitted_n = abs(mode_object.n(max(index, 1), 1))
         
         relative_amplitude_differences(i) = abs(fitted_amplitude / correct_amplitude - 1);
         relative_n_differences(i) = abs(fitted_n / correct_n - 1);
